0b0154bab65b43131de8b71c40d016027dc979f7
kevin-tas-wang <58488929+kevin-tas-wang@users.noreply.github.com>
2025-09-19 09:27:10 +0000

    ch 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '@/app/ui/global.css';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
 
 export default function RootLayout({
   children,
@@ -6,8 +8,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+     <html lang="en">
+      <body>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
+
+ 
